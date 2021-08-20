dafe40f57fc058c995a4c103975fc7718efc493d
xstater <a166224@163.com>
2021-08-21 03:47:04 +0800

    make public

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,18 @@
-mod world;
-mod entity;
-mod components;
-mod group;
-mod query;
-mod sparse_set;
-mod systems;
-mod stage;
-mod resource;
+pub mod world;
+pub mod entity;
+pub mod components;
+pub mod group;
+pub mod query;
+pub mod sparse_set;
+pub mod systems;
+pub mod stage;
+pub mod resource;
 
 pub use entity::{ EntityId,Entities };
 pub use world::World;
 pub use components::Component;
+pub use systems::System;
+pub use stage::Stage;
 
 #[cfg(test)]
 mod tests {
