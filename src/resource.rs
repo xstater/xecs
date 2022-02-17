0cd1948d38b3c61033193d0eb90bd5c4a18d03f1
xstater <a166224@163.com>
2022-02-17 09:06:07 +0800

    added resource read/write

--- src/resource.rs
+++ src/resource.rs
@@ -1,13 +1,26 @@
-use std::{any::Any, fmt::{Debug, Display}, marker::PhantomData, ops::{Deref, DerefMut}, sync::{RwLockReadGuard, RwLockWriteGuard}};
+use std::{fmt::{Debug, Display}, marker::PhantomData, ops::{Deref, DerefMut}, sync::{RwLockReadGuard, RwLockWriteGuard}};
+
+/// The resource trait 
+pub trait Resource : Send + Sync + 'static {}
+impl<T : Send + Sync + 'static> Resource for T {}
+
+impl dyn Resource {
+    pub(in crate) unsafe fn downcast_ref<T : Resource>(&self) -> &T{
+        &*(self as *const dyn Resource as *const T)
+    }
+    pub(in crate) unsafe fn downcast_mut<T : Resource>(&mut self) -> &mut T{
+        &mut *(self as *mut dyn Resource as *mut T)
+    }
+}
 
 /// A read lock gurad for resource
 pub struct ResourceRead<'a,T> {
-    lock : RwLockReadGuard<'a,Option<Box<dyn Any + Send + Sync>>>,
+    lock : RwLockReadGuard<'a,Box<dyn Resource>>,
     _marker : PhantomData<T>
 }
 
-impl<'a,T : 'static + Send + Sync> ResourceRead<'a,T> {
-    pub(in crate) fn from_read(lock : RwLockReadGuard<'a,Option<Box<dyn Any + Send + Sync>>>) -> Self {
+impl<'a,T : Resource> ResourceRead<'a,T> {
+    pub(in crate) fn new(lock : RwLockReadGuard<'a,Box<dyn Resource>>) -> Self {
         ResourceRead {
             lock,
             _marker : Default::default()
@@ -15,27 +28,30 @@ impl<'a,T : 'static + Send + Sync> ResourceRead<'a,T> {
     }
 }
 
-impl<'a,T : 'static + Send + Sync> Deref for ResourceRead<'a,T> {
+impl<'a,T : Resource> Deref for ResourceRead<'a,T> {
     type Target = T;
 
-    fn deref(&self) -> &Self::Target {
-        self.lock.as_ref().unwrap() // This unwrap never fails. It's ensured by outside
-            .downcast_ref::<T>().unwrap() // also
+    fn deref(&self) -> &Self::Target{
+        unsafe {
+            self.lock .downcast_ref::<T>() // safety: safe because we checked type id outside
+        }
     }
 }
 
-impl<'a,T : 'static + Send + Sync + Debug> Debug for ResourceRead<'a,T> {
+impl<'a,T : Resource + Debug> Debug for ResourceRead<'a,T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let data = self.lock.as_ref().unwrap() // This unwrap never fails. It's ensured by outside
-            .downcast_ref::<T>().unwrap();// Also
+        let data = unsafe {
+            self.lock .downcast_ref::<T>() // safety : Safe because we checked type id outside
+        };
         f.debug_struct("ResourceRead").field("lock", data).finish()
     }
 }
 
-impl<'a,T : 'static + Send + Sync + Display> Display for ResourceRead<'a,T> {
+impl<'a,T : Resource + Display> Display for ResourceRead<'a,T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let data = self.lock.as_ref().unwrap() // This unwrap never fails. It's ensured by outside
-            .downcast_ref::<T>().unwrap();// Also
+        let data = unsafe {
+            self.lock .downcast_ref::<T>()// safety: Safe because we checked type id outside
+        };
         data.fmt(f)
     }
 } 
@@ -45,12 +61,13 @@ impl<'a,T : 'static + Send + Sync + Display> Display for ResourceRead<'a,T> {
 
 /// A write lock gurad for resource
 pub struct ResourceWrite<'a,T> {
-    lock : RwLockWriteGuard<'a,Option<Box<dyn Any + Send + Sync>>>,
+    lock : RwLockWriteGuard<'a,Box<dyn Resource>>,
     _marker : PhantomData<T>
 }
 
-impl<'a,T : 'static + Send + Sync> ResourceWrite<'a,T> {
-    pub(in crate) fn from_write(lock : RwLockWriteGuard<'a,Option<Box<dyn Any + Send + Sync>>>) -> Self {
+
+impl<'a,T : Resource> ResourceWrite<'a,T> {
+    pub(in crate) fn new(lock : RwLockWriteGuard<'a,Box<dyn Resource>>) -> Self {
         ResourceWrite{
             lock,
             _marker : Default::default()
@@ -58,34 +75,38 @@ impl<'a,T : 'static + Send + Sync> ResourceWrite<'a,T> {
     }
 }
 
-impl<'a,T : 'static + Send + Sync> Deref for ResourceWrite<'a,T> {
+impl<'a,T : Resource> Deref for ResourceWrite<'a,T> {
     type Target = T;
 
-    fn deref(&self) -> &Self::Target {
-        self.lock.as_ref().unwrap() // This unwrap never fails. It's ensured by outside
-            .downcast_ref::<T>().unwrap() // also
+    fn deref(&self) -> &Self::Target{
+        unsafe {
+            self.lock .downcast_ref::<T>() // safety: safe because we checked type id outside
+        }
     }
 }
 
-impl<'a,T : 'static + Send + Sync> DerefMut for ResourceWrite<'a,T> {
+impl<'a,T : Resource> DerefMut for ResourceWrite<'a,T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        self.lock.as_mut().unwrap() // This unwrap never fails. It's ensured by outside
-            .downcast_mut::<T>().unwrap() // also
+        unsafe {
+            self.lock .downcast_mut::<T>() // safety : safe because we checked type id outside
+        }
     }
 }
 
-impl<'a,T : 'static + Send + Sync + Debug> Debug for ResourceWrite<'a,T> {
+impl<'a,T : Resource + Debug> Debug for ResourceWrite<'a,T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let data = self.lock.as_ref().unwrap() // This unwrap never fails. It's ensured by outside
-            .downcast_ref::<T>().unwrap();// Also
+        let data = unsafe {
+            self.lock .downcast_ref::<T>() // safety : Safe because we checked type id outside
+        };
         f.debug_struct("ResourceRead").field("lock", data).finish()
     }
 }
 
-impl<'a,T : 'static + Send + Sync + Display> Display for ResourceWrite<'a,T> {
+impl<'a,T : Resource + Display> Display for ResourceWrite<'a,T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let data = self.lock.as_ref().unwrap() // This unwrap never fails. It's ensured by outside
-            .downcast_ref::<T>().unwrap();// Also
+        let data = unsafe {
+            self.lock .downcast_ref::<T>()// safety: Safe because we checked type id outside
+        };
         data.fmt(f)
     }
 } 
