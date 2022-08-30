6b8939ff17aa36b697a413e14a50f0afd7baa55e
xstater <a166224@163.com>
2022-08-30 22:46:51 +0800

    added insert & removed

--- src/storage/tests.rs
+++ src/storage/tests.rs
@@ -1,9 +1,25 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{
+    collections::{HashMap, HashSet},
+    sync::Arc,
+};
 
 use crate::{ComponentAny, ComponentStorage, EntityId};
 use parking_lot::RwLock;
 use rand::Rng;
 use xsparseset::{SparseSet, SparseSetHashMap};
+
+// Define a struct to count how many times of called drop
+// use `RwLock` to ensure `Send + Sync`
+struct Test {
+    count: Arc<RwLock<usize>>,
+}
+
+impl Drop for Test {
+    fn drop(&mut self) {
+        let mut count = self.count.write();
+        *count += 1;
+    }
+}
 
 #[test]
 fn basic_storage_dyn() {
@@ -16,11 +32,11 @@ fn basic_storage_dyn() {
     assert_eq!(sparse_set.len(), 1);
     assert!(sparse_set.contains(id));
     {
-        let sparse_set = unsafe { sparse_set.downcast_ref::<SparseSetHashMap<EntityId,char>>() };
+        let sparse_set = unsafe { sparse_set.downcast_ref::<SparseSetHashMap<EntityId, char>>() };
         let result = sparse_set.get(id).copied();
         assert!(result.is_some());
-        assert_eq!(result.unwrap(),'c');
-        assert_eq!(sparse_set.data(),&['c']);
+        assert_eq!(result.unwrap(), 'c');
+        assert_eq!(sparse_set.data(), &['c']);
     }
 
     let id = EntityId::new(14).unwrap();
@@ -31,11 +47,11 @@ fn basic_storage_dyn() {
     assert_eq!(sparse_set.len(), 2);
     assert!(sparse_set.contains(id));
     {
-        let sparse_set = unsafe { sparse_set.downcast_ref::<SparseSetHashMap<EntityId,char>>() };
+        let sparse_set = unsafe { sparse_set.downcast_ref::<SparseSetHashMap<EntityId, char>>() };
         let result = sparse_set.get(id).copied();
         assert!(result.is_some());
-        assert_eq!(result.unwrap(),'b');
-        assert_eq!(sparse_set.data(),&['c','b']);
+        assert_eq!(result.unwrap(), 'b');
+        assert_eq!(sparse_set.data(), &['c', 'b']);
     }
 }
 
@@ -46,7 +62,7 @@ fn rand_storage_dyn() {
     let mut sparse_set: Box<dyn ComponentStorage> = Box::new(sparse_set);
 
     let mut ids = HashMap::new();
-    
+
     let count = 10_000;
     for _ in 0..count {
         loop {
@@ -69,45 +85,28 @@ fn rand_storage_dyn() {
                 // insert any unchecked
                 let mut s = s;
                 // give out the ownership of s to sparseset
-                unsafe {
-                    sparse_set.insert_any_unchecked(id, &mut s as *mut String as *mut _)
-                }
+                unsafe { sparse_set.insert_any_unchecked(id, &mut s as *mut String as *mut _) }
                 std::mem::forget(s);
             }
             break;
         }
     }
 
-    assert_eq!(sparse_set.len(),ids.len());
+    assert_eq!(sparse_set.len(), ids.len());
     {
-        let sparse_set = unsafe {
-            sparse_set.downcast_ref::<SparseSetHashMap<EntityId,String>>()
-        };
-        for (id,s) in ids.into_iter() {
+        let sparse_set = unsafe { sparse_set.downcast_ref::<SparseSetHashMap<EntityId, String>>() };
+        for (id, s) in ids.into_iter() {
             assert!(sparse_set.contains(id));
             let result = sparse_set.get(id);
             assert!(result.is_some());
-            assert_eq!(&s,result.unwrap());
+            assert_eq!(&s, result.unwrap());
         }
     }
 }
 
 #[test]
-fn storage_dyn_drop_test() {
+fn storage_dyn_drop() {
     // This test to ensure all data in storage will be released correctly
-    // Define a struct to count how many times of called drop
-    // use `RwLock` to ensure `Send + Sync`
-    struct Test {
-        count: Arc<RwLock<usize>>
-    }
-
-    impl Drop for Test {
-        fn drop(&mut self) {
-            let mut count = self.count.write();
-            *count += 1;
-        }
-    }
-
     let drop_count = Arc::new(RwLock::new(0));
 
     let mut rng = rand::thread_rng();
@@ -119,16 +118,14 @@ fn storage_dyn_drop_test() {
         let id = EntityId::new(i).unwrap();
         if rng.gen_bool(0.5) {
             let data = Box::new(Test {
-                count: drop_count.clone()
+                count: drop_count.clone(),
             });
             sparse_set.insert_any(id, data);
         } else {
             let mut data = Test {
-                count: drop_count.clone()
+                count: drop_count.clone(),
             };
-            unsafe {
-                sparse_set.insert_any_unchecked(id, &mut data as *mut Test as *mut _)
-            };
+            unsafe { sparse_set.insert_any_unchecked(id, &mut data as *mut Test as *mut _) };
             std::mem::forget(data);
         }
     }
@@ -136,7 +133,109 @@ fn storage_dyn_drop_test() {
     let count = sparse_set.len();
     // trig drop
     std::mem::drop(sparse_set);
-    
+
     let drop_count = drop_count.read();
-    assert_eq!(*drop_count,count);
+    assert_eq!(*drop_count, count);
+}
+
+#[test]
+fn storage_concrete_insert_drop() {
+    // This test to ensure all data in storage will be released correctly
+    let drop_count = Arc::new(RwLock::new(0));
+
+    let mut rng = rand::thread_rng();
+    let sparse_set: SparseSetHashMap<EntityId, Test> = SparseSet::default();
+    let mut sparse_set: Box<dyn ComponentStorage> = Box::new(sparse_set);
+
+    let mut ids = HashSet::new();
+
+    let count = 100_000;
+    for _ in 1..count {
+        let id = rng.gen_range(2..10000000);
+        if ids.contains(&id) {
+            continue;
+        }
+        ids.insert(id);
+        let id = EntityId::new(id).unwrap();
+        sparse_set.insert(
+            id,
+            Test {
+                count: drop_count.clone(),
+            },
+        );
+    }
+
+    let count = sparse_set.len();
+    // trig drop
+    std::mem::drop(sparse_set);
+
+    let drop_count = drop_count.read();
+    assert_eq!(*drop_count, count);
+}
+
+#[test]
+fn storage_concrete_insert_remove_drop() {
+    // This test to ensure all data in storage will be released correctly
+    let drop_count = Arc::new(RwLock::new(0));
+
+    let mut rng = rand::thread_rng();
+    let sparse_set: SparseSetHashMap<EntityId, Test> = SparseSet::default();
+    let mut sparse_set: Box<dyn ComponentStorage> = Box::new(sparse_set);
+
+    let mut ids = HashSet::new();
+
+    let count = 100_000;
+    for _ in 1..count {
+        let id = rng.gen_range(2..10000000);
+        if ids.contains(&id) {
+            continue;
+        }
+        ids.insert(id);
+        let id = EntityId::new(id).unwrap();
+        sparse_set.insert(
+            id,
+            Test {
+                count: drop_count.clone(),
+            },
+        );
+    }
+
+    // remove something and count
+    let mut itr = ids.iter().copied();
+    let count = rng.gen_range(1_000..count);
+    for _ in 0..count {
+        let id_to_be_removed = itr.next().unwrap();
+        let id_to_be_removed = EntityId::new(id_to_be_removed).unwrap();
+        let _ = sparse_set.remove::<Test>(id_to_be_removed);
+    }
+
+    {
+        let drop_count = drop_count.read();
+        assert_eq!(count, *drop_count)
+    }
+}
+
+#[test]
+fn storage_concrete_insert_remove() {
+    let mut rng = rand::thread_rng();
+    let sparse_set: SparseSetHashMap<EntityId, char> = SparseSet::default();
+    let mut sparse_set: Box<dyn ComponentStorage> = Box::new(sparse_set);
+
+    let mut values = HashMap::new();
+
+    let count = 100_000;
+    for _ in 0..count {
+        let id = rng.gen_range(1..10000000);
+        let id = EntityId::new(id).unwrap();
+        let ch = rng.gen_range('a'..='z');
+        let v1 = values.insert(id,ch);
+        let v2 = sparse_set.insert(id,ch);
+        assert_eq!(v1,v2);
+    }
+
+    for (id,ch) in values {
+        let result = sparse_set.remove::<char>(id);
+        assert!(result.is_some());
+        assert_eq!(result.unwrap(),ch);
+    }
 }
