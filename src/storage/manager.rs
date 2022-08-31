d36b60e3ec785603b8252d4b6542b01344d545f2
xstater <a166224@163.com>
2022-09-01 00:53:12 +0800

    some tries

--- src/storage/manager.rs
+++ src/storage/manager.rs
@@ -26,6 +26,12 @@ impl<T> Edges<T> {
             .unwrap_or_default()
     }
 
+    fn reached_with_data(&self, node: StorageId) -> Vec<(StorageId,&T)> {
+        self.edges.get(&node)
+            .map(|node|node.iter().map(|(k,v)|(k.clone(),v)).collect())
+            .unwrap_or_default()
+    }
+
     fn insert(&mut self, start: StorageId, to: StorageId, data: T) {
         if let Some(node) = self.edges.get_mut(&start) {
             node.insert(to, data);
@@ -77,22 +83,61 @@ impl Manager {
         StorageId::Group(id)
     }
 
+    fn is_owned(&self,storage_id: StorageId) -> bool{
+        let parents = self.back_edges.reached(storage_id);
+        for parent in parents {
+            let is_owned = self.forward_edges
+                .get(parent, storage_id)
+                .copied()
+                .unwrap_or_else(||unreachable!());
+            if is_owned {
+                return true;
+            }
+        }
+        false
+    }
+
     pub fn contains(&self, storage_id: StorageId) -> bool {
         self.nodes.contains_key(&storage_id)
     }
 
     /// # Safety
     /// * Safe when `!self.contains(storage_id)`
-    pub unsafe fn insert(&mut self,storage_id: StorageId,storage: RwLock<Box<dyn ComponentStorage>>){
+    pub unsafe fn insert_storage(&mut self,storage_id: StorageId,storage: RwLock<Box<dyn ComponentStorage>>){
         self.nodes.insert(storage_id,Node::Storage(storage));
     }
 
     /// # Safety
+    /// * Safe when `!self.contains(storage_id)`
+    unsafe fn insert_group(&mut self,storage_id: StorageId) {
+        self.nodes.insert(storage_id, Node::Group);
+    }
+
+    /// # Safety
     /// * `self.contains(storage_id1) && self.contains(storage_id2)`
-    /// * todo
+    /// * `!self.is_owned(storage_id1) && !self.is_owned(storage_id2)`
     pub unsafe fn make_full_owning_group(&mut self,storage_id1: StorageId, storage_id2: StorageId) -> StorageId {
         let group_id = self.next_group_id();
-
+        self.insert_group(group_id);
+        self.forward_edges.insert(group_id, storage_id1, true);
+        self.back_edges.insert(storage_id1,group_id,());
+        self.forward_edges.insert(group_id, storage_id2, true);
+        self.back_edges.insert(storage_id2,group_id,());
+        self.rearrange_group(group_id);
         group_id
+    }
+
+    pub unsafe fn rearrange_group(&self,group_id: StorageId) {
+        if let Node::Storage(_) = self.nodes.get(&group_id).unwrap_or_else(|| unreachable!()) {
+            return;
+        }
+        let children = self.forward_edges.reached_with_data(group_id);
+        for (storage_id,is_owned) in children.into_iter().map(|(id,f)|(id,*f)) {
+            if !is_owned {
+                continue;
+            }
+            self.rearrange_group(storage_id)
+        }
+        todo!()
     }
 }
