aa927745fd96551acd102eae5d762e744d408da0
xstater <a166224@163.com>
2022-09-06 17:02:08 +0800

    changed doc to chinese

--- src/world.rs
+++ src/world.rs
@@ -1,21 +1,24 @@
 use parking_lot::RwLock;
-use crate::entity::EntityManager;
+use crate::{entity::EntityManager, Archetype};
 
 
-/// The core of XECS
+/// XECS的核心
 pub struct World {
     next_other_storage_id: u64,
     entities: RwLock<EntityManager>,
+    archetypes: Vec<RwLock<Archetype>>
 }
 
 impl World {
-    /// Create a new empty World
+    /// 创建一个空的World
     pub fn new() -> Self {
         World {
             next_other_storage_id: 0,
             entities: RwLock::new(EntityManager::new()),
+            archetypes: Vec::new()
         }
     }
+
 }
 
 #[cfg(test)]
