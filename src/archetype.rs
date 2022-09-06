c31cac89d098671a936323fa133ac646d47d425c
xstater <a166224@163.com>
2022-09-06 16:30:26 +0800

    added more functions

--- src/archetype.rs
+++ src/archetype.rs
@@ -2,8 +2,8 @@ mod iter;
 #[cfg(test)]
 mod tests;
 
-use std::{any::TypeId, collections::HashMap};
 use crate::{dyn_type_vec::DynTypeVec, Component, ComponentTypeId, EntityId};
+use std::{any::TypeId, collections::HashMap, ops::Range};
 
 /// 具有相同Component组合类型的entity的容器
 pub struct Archetype {
@@ -68,16 +68,40 @@ impl Archetype {
     }
 
     /// 获得entity在Archetype中的index
-    pub fn get_index(&self,entity_id: EntityId) -> Option<usize> {
+    pub fn get_index(&self, entity_id: EntityId) -> Option<usize> {
         self.sparse.get(&entity_id).copied()
     }
 
+    /// 获得所有的storages
     pub fn storages_ref(&self) -> &[Box<dyn DynTypeVec>] {
         &self.storages
     }
 
+    /// 获得所有的storages
     pub fn storages_mut(&mut self) -> &mut [Box<dyn DynTypeVec>] {
         &mut self.storages
+    }
+
+    /// 根据component_id获得storage
+    pub fn storage_ref(&self,component_id: ComponentTypeId) -> Option<&dyn DynTypeVec> {
+        let index = self.types.iter()
+            .enumerate()
+            .find(|(_,cid)|**cid == component_id)
+            .map(|(index,_)|index)?;
+        Some(unsafe {
+            self.storages.get_unchecked(index)
+        }.as_ref())
+    }
+
+    /// 根据component_id获得storage
+    pub fn storage_mut(&mut self,component_id: ComponentTypeId) -> Option<&mut dyn DynTypeVec> {
+        let index = self.types.iter()
+            .enumerate()
+            .find(|(_,cid)|**cid == component_id)
+            .map(|(index,_)|index)?;
+        Some(unsafe {
+            self.storages.get_unchecked_mut(index)
+        }.as_mut())
     }
 
     /// 插入buffer中的数据到Archetype中
@@ -137,20 +161,50 @@ impl Archetype {
             }
         };
     }
-    
+
+    /// 插入大量数据到Archetype中
+    /// # Details
+    /// `ids`和`data`中的数据都会被移动到Archetype中
+    /// # Safety
+    /// * Archetype中不能存在`ids`中的id
+    /// * `ids`和`data`中的所有`DynTypeVec`必须一样长
+    /// * `data`必须和`types`长度相同
+    /// * `data`中每个`DynTypeVec`的`type_id`方法必须和Archetype中storages的`type_id`返回值相同，且顺序一致
+    /// * `data`中所有指针必须有效
+    pub unsafe fn insert_any_batch_unchecked(
+        &mut self,
+        ids: &mut Vec<EntityId>,
+        data: &[*mut dyn DynTypeVec],
+    ) {
+        let mut index = self.len();
+        for id in ids.iter().copied() {
+            self.sparse.insert(id, index);
+            index += 1;
+        }
+
+        self.entities.append(ids);
+
+        for i in 0..data.len() {
+            let data = *data.get_unchecked(i);
+            let storage = self.storages.get_unchecked_mut(i);
+
+            storage.append_any_unchecked(&mut *data);
+        }
+    }
+
     /// 从Archetype中移除数据
     /// # Remarks
     /// 被移除的数据将会被`drop`
     /// # Safety
     /// * `entity_id`必须存在于archetype中
-    pub unsafe fn remove_and_drop_unchecked(&mut self,entity_id: EntityId) {
+    pub unsafe fn remove_and_drop_unchecked(&mut self, entity_id: EntityId) {
         let index = self.sparse.get(&entity_id).copied().unwrap_unchecked();
         if index != self.len() {
             // swap to last
             let last_id = self.entities.last().unwrap_unchecked();
             *self.sparse.get_mut(last_id).unwrap_unchecked() = index;
             let last_index = self.len() - 1;
-            self.entities.swap(index,last_index);
+            self.entities.swap(index, last_index);
             for storage in &mut self.storages {
                 storage.swap(index, last_index)
             }
@@ -167,14 +221,14 @@ impl Archetype {
     /// 被移除的数据将会被`drop`
     /// # Safety
     /// * `entity_id`必须存在于archetype中
-    pub unsafe fn remove_and_forget_unchecked(&mut self,entity_id: EntityId) {
+    pub unsafe fn remove_and_forget_unchecked(&mut self, entity_id: EntityId) {
         let index = self.sparse.get(&entity_id).copied().unwrap_unchecked();
         if index != self.len() {
             // swap to last
             let last_id = self.entities.last().unwrap_unchecked();
             *self.sparse.get_mut(last_id).unwrap_unchecked() = index;
             let last_index = self.len() - 1;
-            self.entities.swap(index,last_index);
+            self.entities.swap(index, last_index);
             for storage in &mut self.storages {
                 storage.swap(index, last_index)
             }
@@ -186,6 +240,28 @@ impl Archetype {
         }
     }
 
+    /// 移除Archetype中的一段数据
+    /// # Details
+    /// 被移除的数据会装在Vec里返回
+    /// # Safety
+    /// * `range`必须在范围内
+    pub unsafe fn remove_batch_unchecked(&mut self, range: Range<usize>) -> (Vec<EntityId>,Vec<Box<dyn DynTypeVec>>){
+        let removed_ids = {
+            let mut removed_ids = self.entities.split_off(range.start);
+            let mut remain_ids = removed_ids.split_off(range.end - range.start);
+            self.entities.append(&mut remain_ids);
+            removed_ids
+        };
+
+        for id in removed_ids.iter() {
+            self.sparse.remove(id);
+        }
+
+        let removed_data = self.storages.iter_mut()
+            .map(|storage| storage.remove_range(range.clone()))
+            .collect::<Vec<_>>();
+        (removed_ids,removed_data)
+    }
 
     /// 获得Archetype中entity_id对应数据
     /// # Details
@@ -193,7 +269,7 @@ impl Archetype {
     /// # Safety
     /// * `entity_id`必须存在于Archetype中
     /// * `data_ptrs.len() == self.types().len()`
-    pub unsafe fn get_unchecked(&self,entity_id: EntityId, data_ptrs: &mut [*const u8]) {
+    pub unsafe fn get_unchecked(&self, entity_id: EntityId, data_ptrs: &mut [*const u8]) {
         let index = self.sparse.get(&entity_id).copied().unwrap_unchecked();
         for i in 0..self.storages.len() {
             let storage = self.storages.get_unchecked(i);
@@ -208,7 +284,7 @@ impl Archetype {
     /// # Safety
     /// * `entity_id`必须存在于Archetype中
     /// * `data_ptrs.len() == self.types().len()`
-    pub unsafe fn get_mut_unchecked(&mut self,entity_id: EntityId, data_ptrs: &mut [*mut u8]) {
+    pub unsafe fn get_mut_unchecked(&mut self, entity_id: EntityId, data_ptrs: &mut [*mut u8]) {
         let index = self.sparse.get(&entity_id).copied().unwrap_unchecked();
         for i in 0..self.storages.len() {
             let storage = self.storages.get_unchecked_mut(i);
@@ -216,4 +292,5 @@ impl Archetype {
             *data_ptrs.get_unchecked_mut(i) = ptr;
         }
     }
+
 }
