7e954def2dd5c8ca731cfd04b0c27c258a849cf8
xstater <a166224@163.com>
2022-10-11 14:07:43 +0800

    temply saved

--- src/components/full_owning.rs
+++ src/components/full_owning.rs
@@ -13,6 +13,22 @@ where
     A: Storage,
     B: Storage,
 {
+    pub fn new(storage_a: A, storage_b: B) -> Self {
+        let mut group = FullOwning {
+            len: 0,
+            storage_a,
+            storage_b,
+        };
+
+        let ids_a = group.storage_a.ids().to_vec();
+
+        for id_a in ids_a {
+            group.move_to_group(id_a);
+        }
+
+        group
+    }
+
     /// Move 2 items to group and return their index in group
     /// # Returns
     /// Return Some(index) if move successfully or they are already in group
@@ -27,13 +43,8 @@ where
             let index = self.len;
             self.len += 1;
 
-            // # Safety
-            // index_a and index_b are from `get_index`, so it always in range
-            // len is always less than the `min(storage_a.len, storage_b.len)`
-            unsafe {
-                self.storage_a.swap_by_index_unchecked(index_a, index);
-                self.storage_b.swap_by_index_unchecked(index_b, index);
-            }
+            self.storage_a.swap_by_index(index_a, index);
+            self.storage_b.swap_by_index(index_b, index);
 
             Some(index)
         } else {
@@ -46,17 +57,10 @@ where
     /// Return None if they are not in group
     fn move_out_from_group(&mut self, index: usize) -> Option<usize> {
         if index < self.len {
-            // # No Panics
-            // never overflow here, because when `self.len == 0` will never enter this branch
             self.len -= 1;
 
-            // # Safety
-            // index_a and index_b are from `get_index`, so it always in range
-            // len is always less than the `min(storage_a.len, storage_b.len)`
-            unsafe {
-                self.storage_a.swap_by_index_unchecked(index, self.len);
-                self.storage_b.swap_by_index_unchecked(index, self.len);
-            }
+            self.storage_a.swap_by_index(index, self.len);
+            self.storage_b.swap_by_index(index, self.len);
 
             Some(self.len)
         } else {
@@ -133,26 +137,66 @@ where
     }
 
     fn insert_batch(&mut self, ids: Vec<EntityId>, data: Vec<Self::Item>) {
-        let (mut a,mut b) :(Vec<_>,Vec<_>) = data.into_iter().unzip();
-        let mut ids_a = ids.clone();
-        let mut ids_b = ids;
-
-        
+        // just simple insert it all
+        for (id,data) in ids.into_iter().zip(data.into_iter()) {
+            self.insert(id, data)
+        }
     }
 
     fn get(&self, id: EntityId) -> Option<&Self::Item> {
-        todo!()
+        todo!("Cannot impl")
     }
 
     fn get_mut(&mut self, id: EntityId) -> Option<&mut Self::Item> {
-        todo!()
+        todo!("Cannot impl")
     }
 
     fn ids(&self) -> &[EntityId] {
-        todo!()
+        &self.storage_a.ids()[..self.len]
     }
 
     fn data(&self) -> &[Self::Item] {
-        todo!()
+        todo!("Cannot impl")
+    }
+}
+
+/// 1 2 3 4
+/// 1 2 3 5
+
+#[cfg(test)]
+mod tests {
+    use xsparseset::SparseSetHashMap;
+
+    use crate::{EntityId, components::{full_owning::FullOwning, Storage}};
+
+    #[test]
+    fn basic() {
+        let mut s1 = SparseSetHashMap::default();
+        let mut s2 = SparseSetHashMap::default();
+
+        s1.insert(EntityId::new(2).unwrap(), 2);
+        s1.insert(EntityId::new(5).unwrap(), 1);
+        s1.insert(EntityId::new(1).unwrap(), 3);
+        s1.insert(EntityId::new(7).unwrap(), 4);
+
+        assert_eq!(s1.ids(), [2,5,1,7].map(|x|EntityId::new(x).unwrap()).as_ref());
+
+        s2.insert(EntityId::new(5).unwrap(), 'c');
+        s2.insert(EntityId::new(3).unwrap(), 'b');
+        s2.insert(EntityId::new(2).unwrap(), 'a');
+
+        assert_eq!(s2.ids(), [5,3,2].map(|x|EntityId::new(x).unwrap()).as_ref());
+
+        let mut group = FullOwning::new(s1,s2);
+
+        assert_eq!(group.len(), 2);
+
+        for index in 0..group.len() {
+            let id_a = group.storage_a.ids()[index];
+            let id_b = group.storage_b.ids()[index];
+
+            assert_eq!(id_a, id_b);
+        }
+
     }
 }
